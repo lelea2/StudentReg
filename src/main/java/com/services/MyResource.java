1753ecbbb798315567bcc1144da28c5aadb13838
lelea2 <daomaikhanh1988@yahoo.com>
2015-09-06 18:00:37 -0700

    Initial check in for jersey set up

--- src/main/java/com/services/MyResource.java
+++ src/main/java/com/services/MyResource.java
@@ -18,8 +18,8 @@ public class MyResource {
      * @return String that will be returned as a text/plain response.
      */
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String getIt() {
-        return "Got it!";
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getResources() {
+        return "{\"id\": \"abc-123\", \"firstName\": \"Khanh\"}";
     }
 }
