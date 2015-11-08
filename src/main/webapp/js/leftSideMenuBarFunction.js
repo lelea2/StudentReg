a02ff2632721a9b9ca011c829f1f44e2eb49f430
leonasber <168288646@qq.com>
2015-11-08 00:17:41 -0800

    admin pages & separate main page body

--- src/main/webapp/js/leftSideMenuBarFunction.js
+++ src/main/webapp/js/leftSideMenuBarFunction.js
@@ -7,29 +7,13 @@ $(document).ready(function(){
         $(".o_hFunction").slideToggle("fast");
             n = n * -1;
             if (n == -1){
-                $(".rightSide").css("width","80%")
-                $(".pageSzie").css("width","80%");
+                $(".rightSide").css("width","100%")
+
             }
             if (n == 1){
-                $(".rightSide").css("width","100%")
-                $(".pageSize").css("width","100%");
+                $(".rightSide").css("width","85%")
+
             }
     });
 
-
-    $("#red").click(function(){
-            $(".pageSize").hide("fast");
-            $("#pageA").show("fast");
-        }
-    );
-    $("#orange").click(function(){
-        $(".pageSize").hide("fast");
-        $("#pageB").show("fast");
-    }
-    );
-    $("#green").click(function(){
-        $(".pageSize").hide("fast");
-        $("#pageC").show("fast");
-    }
-    );
 });
